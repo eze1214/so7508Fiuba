b77d64dd06a7d35de54669f86d35a7b3f108434a
llocani <leandro@debian.agea.com.ar>
2017-04-24 14:27:01 -0300

    se agrega la comparación para que la fecha no sea en el futuro ni mas vieja que 15 dias

--- ejecutables/validar_fecha.sh
+++ ejecutables/validar_fecha.sh
@@ -1,14 +1,21 @@
 valido="false"
-#regex="^[0-9]{4}[0-1][0-9][0-3][0-9]"
+regex="^[0-9]{4}[0-1][0-9][0-3][0-9]"
 #echo "entrada = $1"
 if [[ $1 =~ $regex  ]]
 	then
 	date "+%Y%mm%dd" -d "$1" > /dev/null 2>&1	
 	is_valid=$?
 	#echo "is_valid = $is_valid"
+	date_argumento=$1
+	date_argumento="$(date +%s -d $date_argumento)"
+	#echo "date1 = $date_argumento"
+	#date2="$(date +%Y%m%d --date=\"now - 15 days\")"
+	date2=`date --date="15 days ago" +%Y%m%d`
+	date_15_dias="$(date +%s -d $date2)"
+	date_hoy=`date --date="now" +%Y%m%d`
+	date_hoy="$(date +%s -d $date_hoy)"
 
-	#if [ $is_valid == 0 ] && [ date -d "now - 15 days" -lt date -d "$1" ] && [ date -d "$1" -lt date -d "now" ]
-	if [ $is_valid == 0 ]
+	if [ $is_valid == 0 ] && [ $date_15_dias -lt $date_argumento ] && [ $date_argumento -lt $date_hoy ]
 		then
 		valido="true"
 	fi
