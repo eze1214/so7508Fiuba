e4cba99b398738a32a429ea3dd8a5cdc5f5bf6ba
bessalex <bessalext300@gmail.com>
2017-05-08 14:59:57 +0000

    Startd y stopd no utilizan archivo, TODO inicializar no lo genere

--- ejecutables/startd.sh
+++ ejecutables/startd.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+SRV="daemon"
+SERV=$(ps -e |grep ${SRV} |awk '{print $1}')
 
-
-if [ -f /tmp/daemon.pid ]; then
-    PID=$(cat /tmp/daemon.pid )
-    echo "El demonio está en ejecución, PID: $PID"
+if [ ${SERV} ]; then
+#    PID=$(cat /tmp/daemon.pid )
+    echo "El demonio está en ejecución, PID: ${SERV}"
     exit 3
 fi
 
-$BINARIOS/daemon.sh  & echo $! >> /tmp/daemon.pid
+$BINARIOS/daemon.sh & 
 
+SERV=$(ps -e |grep ${SRV} |awk '{print $1}')
 # Leyendo el archivo obtengo el PID --> así es mucho más facil matarlo
-PID=$(cat /tmp/daemon.pid)
+#PID=$(cat /tmp/daemon.pid)
 
-echo -e ".. Demonio Corriendo ..  PID: $PID"
+echo -e ".. Demonio Corriendo ..  PID: ${SERV}"
--- ejecutables/stopd.sh
+++ ejecutables/stopd.sh
@@ -6,23 +6,26 @@
 #    export BINARIOS=`grep -A 0 BINARIOS $CONFIG | sed "s/\(^.*\)\(=.*\)\(=.*\)\(=.*\)/\2/g" | sed s/=//g`
 
 #source $BINARIOS/inicializaciones.sh
+SRV="daemon"
+SERV=$(ps -e |grep ${SRV} |awk '{print $1}')
 
-
+# echo "fin ${SERV}"
 
 #export GRUPO5_MAESTRO_DE_BANCOS="maestro_de_bancos.csv"
-if [ ! -f /tmp/daemon.pid ]; then
+if [ ! ${SERV} ]; then
     echo "El demonio NO está corriendo "
     exit 3
 fi
 
-PID=$(cat /tmp/daemon.pid )
+#PID=$(cat /tmp/daemon.pid )
 
-kill -9 $PID 2> /dev/null
+#kill -9 $PID 2> /dev/null
+kill -9 ${SERV} 2> /dev/null
 
 if [ $? -eq 0 ]; then
-  echo "..Deteniendo Demonio, PID: $PID .. "
+  echo "..Deteniendo Demonio, PID: ${SERV} .. "
 else
   echo "El Demonio NO está corriendo "
 fi
 
-rm /tmp/daemon.pid 2> /dev/null
+#rm /tmp/daemon.pid 2> /dev/null
